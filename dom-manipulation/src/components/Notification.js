409c9dffc1d34e84f7435c139b5c448d0e83efff
lenmorelms <72908654+lenmorereal@users.noreply.github.com>
2025-01-16 00:23:16 +0200

    Create Notification.js

---
+++ dom-manipulation/src/components/Notification.js
@@ -0,0 +1,8 @@
+// src/components/Notification.js
+import React from 'react';
+
+function Notification({ message }) {
+  return <div className="notification">{message}</div>;
+}
+
+export default Notification;
